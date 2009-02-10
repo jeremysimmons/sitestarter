fd75896b4a96f595925518adcbada60fb1118851
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2009-02-10 02:29:12 +0000

    Renamed Build.bat to Build-Debug-Release.bat Added NAnt files Messages to log files now get escaped so they don't break the XML

--- Src/App/SoftwareMonkeys.SiteStarter.Web/AssemblyInfo.cs
+++ Src/App/SoftwareMonkeys.SiteStarter.Web/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.0.73")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.73")]
+[assembly: AssemblyVersionAttribute("1.0.0.75")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.75")]
 
--- Src/App/SoftwareMonkeys.SiteStarter/AssemblyInfo.cs
+++ Src/App/SoftwareMonkeys.SiteStarter/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.0.181")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.181")]
+[assembly: AssemblyVersionAttribute("1.0.0.183")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.183")]
 
