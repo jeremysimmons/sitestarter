e409bbe3f91d2775799818f540caa2eaf399f3a7
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2009-03-07 08:18:26 +0000

    Adding SoftwareMonkeys.SiteStarter.Business.Tests

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/Entities/TestArticlePage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Tests.Entities
+{
+    [DataStore("Testing")]
+    public class TestArticlePage : BaseEntity
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private TestArticle article;
+        [EntityReferences(MirrorName="Pages")]
+        public TestArticle Article
+        {
+            get { return article; }
+            set { article = value; }
+        }
+    }
+}
