62d13850c4469e35ffe2bac371fed8148620cbbb
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2009-03-07 08:18:26 +0000

    Adding SoftwareMonkeys.SiteStarter.Business.Tests

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/Entities/EntityTwo.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Tests.Entities
+{
+    [DataStore("Testing")]
+    public class EntityTwo : BaseEntity
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Guid[] oneIDs;
+        public Guid[] OneIDs
+        {
+            get { return oneIDs; }
+            set { oneIDs = value; }
+        }
+    }
+}
