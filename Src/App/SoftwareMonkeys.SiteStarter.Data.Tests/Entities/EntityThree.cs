7cfaaa742bc11df51357233af85c88eafa101acd
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2009-03-23 02:44:29 +0000

    Adding SoftwareMonkeys.SiteStarter.Data.Tests.Entities.EntityThree.cs and EntityFour.cs

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Data.Tests/Entities/EntityThree.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Data.Tests.Entities
+{
+    [DataStore("Testing")]
+    public class EntityThree : BaseEntity
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Guid[] referencedEntityIDs = new Guid[] {};
+	[EntityIDReferences(MirrorName="ReferencedEntityIDs",
+		EntitiesPropertyName="ReferencedEntities",
+		IDsPropertyName="ReferencedEntityIDs")]
+        public Guid[] ReferencedEntityIDs
+        {
+            get {
+		//if (referencedEntities != null)
+		//	return Collection<BaseEntity>.GetIDs(referencedEntities);
+		return referencedEntityIDs; }
+            set {
+		referencedEntityIDs = value;
+		if (referencedEntityIDs == null || (referencedEntities != null && !referencedEntityIDs.Equals(Collection<BaseEntity>.GetIDs(referencedEntities))))
+			referencedEntities = null;
+		}
+        }
+
+	private EntityFour[] referencedEntities;
+	[EntityReferences(ExcludeFromDataStore=true,
+		MirrorName="ReferencedEntities",
+		EntitiesPropertyName="ReferencedEntities",
+		IDsPropertyName="ReferencedEntityIDs")]
+        public EntityFour[] ReferencedEntities
+	{
+		get { return referencedEntities; }
+		set { referencedEntities = value;
+		//	referencedEntityIDs = Collection<BaseEntity>.GetIDs(referencedEntities);
+		}
+	}
+    }
+}
