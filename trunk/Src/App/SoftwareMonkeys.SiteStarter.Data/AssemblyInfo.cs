68f3ed2462853f09176735243682b3527e9c69ec
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2009-05-27 13:12:22 +0000

    Recompiled DLLs Removed obsolete DLLs

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Data/AssemblyInfo.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Data/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.6.261")]
-[assembly: AssemblyFileVersionAttribute("1.0.6.261")]
+[assembly: AssemblyVersionAttribute("1.0.6.263")]
+[assembly: AssemblyFileVersionAttribute("1.0.6.263")]
 
