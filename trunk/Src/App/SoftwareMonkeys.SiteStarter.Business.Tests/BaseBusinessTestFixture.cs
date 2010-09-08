521d3a394c9fc57f670de3ea01c70226bfd7a495
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-09-08 05:14:22 +0000

    Added Initialize function to BaseBusinessTestFixture to initialize the business strategies

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/BaseBusinessTestFixture.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/BaseBusinessTestFixture.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using SoftwareMonkeys.SiteStarter.Data.Tests;
+using NUnit.Framework;
 
 namespace SoftwareMonkeys.SiteStarter.Business.Tests
 {
-	/// <summary>
-	/// Description of BaseBusinessTestFixture.
-	/// </summary>
 	public class BaseBusinessTestFixture : BaseDataTestFixture
 	{
+		[SetUp]
+		public void Initialize()
+		{
+			new StrategyInitializer().Initialize();
+		}
+		
 		public BaseBusinessTestFixture()
 		{
 			
