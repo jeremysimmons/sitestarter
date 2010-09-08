5af85e4b560c74b93a1014ca9fbf4fb735f667b4
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-09-08 05:10:09 +0000

    Removed DataStore property from BaseStrategy (not needed)

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Business/BaseStrategy.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business/BaseStrategy.cs
@@ -7,23 +7,9 @@ namespace SoftwareMonkeys.SiteStarter.Business
 	/// <summary>
 	/// Defines the base class used by all business strategy components.
 	/// </summary>
-	public class BaseStrategy<T>
-		where T : IEntity
+	[Strategy(false)]
+	public class BaseStrategy : IStrategy
 	{		
-		/// <summary>
-		/// Gets the data store that corresponds with the specified type and virtual server.
-		/// </summary>
-		public IDataStore DataStore
-		{
-			get
-			{
-				return DataAccess.Data.Stores
-				[
-					typeof(T)
-				];
-			}
-		}
-		
 		public BaseStrategy()
 		{
 		}
