2dd000b17ba8c5a70261f587b3ca4ed66b4fc43b
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-09-18 05:04:55 +0000

    Moved functions from LogUtilities to LogThreader (used to split large logs into smaller threads) Added LogGrower and LogGrowerTests - used to turn standard log threads into log trees, which better represent the tree link call structure of the application and allow logs to easily be rendered into tree like structures

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Diagnostics.Tests/LogGrowerTests.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using NUnit.Framework;
+using SoftwareMonkeys.SiteStarter.Tests;
+using System.Xml;
+
+namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
+{
+	[TestFixture]
+	public class LogGrowerTests : BaseDiagnosticsTestFixture
+	{
+		public LogGrowerTests()
+		{
+		}
+		
+		[Test]
+		public void Test_CreateTreeNode()
+		{
+			XmlDocument targetDocument = new XmlDocument();
+			
+			XmlDocument sourceDoc = CreateMockThreadDocument();
+			
+			XmlElement sourceElement = (XmlElement)sourceDoc.DocumentElement.ChildNodes[0];
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestingPath(this));
+			
+			XmlElement newElement = grower.CreateTreeNode(targetDocument, sourceElement);
+			
+			Assert.AreEqual(7, newElement.Attributes.Count, "Invalid number of sub nodes found in the entry.");
+			
+			for (int i = 0; i < 7; i++)
+			{
+				Assert.AreEqual(sourceElement.ChildNodes[i].InnerText, newElement.Attributes[i].InnerText, "The inner text of the sub elements wasn't transferred to the attributes.");
+			}
+		}
+		
+		/// <summary>
+		/// Tests the AddNodeToTree function where the previous element passed is null.
+		/// </summary>
+		[Test]
+		public void Test_AddNodeToTree_NullPrevious()
+		{
+			XmlDocument treeDoc = CreateMockThreadDocument();
+			
+			XmlElement element = CreateMockTreeElement(treeDoc, 1);
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			grower.AddNodeToTree(treeDoc, null, element);
+			
+			int expected = 2; // Existing mock entry plus new entry = 2 total
+			Assert.AreEqual(expected, treeDoc.DocumentElement.ChildNodes.Count, "The element wasn't added to the document element.");
+		}
+		
+		/// <summary>
+		/// Tests the AddNodeToTree function where the previous element passed is the parent of the new element.
+		/// </summary>
+		[Test]
+		public void Test_AddNodeToTree_PreviousIsParent()
+		{
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement element = CreateMockTreeElement(treeDoc, 2);
+			
+			XmlElement parentElement = (XmlElement)treeDoc.DocumentElement.ChildNodes[0];
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			grower.AddNodeToTree(treeDoc, parentElement, element);
+			
+			Assert.AreEqual(1, treeDoc.DocumentElement.ChildNodes.Count, "Document element has invalid number of child elements.");
+			Assert.AreEqual(1, treeDoc.DocumentElement.ChildNodes[0].ChildNodes.Count, "New element wasn't added to parent.");
+		}
+			
+		/// <summary>
+		/// Tests the AddNodeToTree function where the previous element passed is a sibling of the new element.
+		/// </summary>
+		[Test]
+		public void Test_AddNodeToTree_PreviousIsSibling()
+		{
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement element = CreateMockTreeElement(treeDoc, 1);
+			
+			XmlElement parentElement = (XmlElement)treeDoc.DocumentElement.ChildNodes[0];
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			grower.AddNodeToTree(treeDoc, parentElement, element);
+			
+			Assert.AreEqual(2, treeDoc.DocumentElement.ChildNodes.Count, "Document element has invalid number of child elements.");
+		}
+		
+		
+			
+		/// <summary>
+		/// Tests the AddNodeToTree function where the previous element passed is indented further than the new element.
+		/// </summary>
+		[Test]
+		public void Test_AddNodeToTree_PreviousIsDeeper()
+		{
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement deeperElement = CreateMockTreeElement(treeDoc, 2);
+			
+			treeDoc.DocumentElement.ChildNodes[0].AppendChild(deeperElement);
+			
+			XmlElement element = CreateMockTreeElement(treeDoc, 1);
+						
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			grower.AddNodeToTree(treeDoc, deeperElement, element);
+			
+			int expectedCount = 2; // default one plus new one = 2
+			Assert.AreEqual(expectedCount, treeDoc.DocumentElement.ChildNodes[0].ChildNodes.Count, "Invalid number of elements. New element wasn't put in correct location.");
+		}
+
+		/// <summary>
+		/// Tests the GetParent function where the previous node is the parent of the new node.
+		/// </summary>		
+		[Test]
+		public void Test_GetParent_PreviousIsParent()
+		{
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement newNode = CreateMockTreeElement(treeDoc, 2);
+			
+			XmlElement previous = (XmlElement)treeDoc.DocumentElement.ChildNodes[0];
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			
+			XmlElement parent = grower.GetParent(previous, newNode);
+			
+			Assert.AreEqual(previous, parent, "Incorrect parent returned.");
+		}
+		
+		/// <summary>
+		/// Tests the GetParent function where the previous node is at a deeper indent level than the new node.
+		/// </summary>		
+		[Test]
+		public void Test_GetParent_PreviousIsDeeper()
+		{
+			
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement previous = CreateMockTreeElement(treeDoc, 2);
+			
+			treeDoc.DocumentElement.ChildNodes[0].AppendChild(previous);
+			
+			XmlElement newNode = CreateMockTreeElement(treeDoc, 1);
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			
+			XmlElement parent = grower.GetParent(previous, newNode);
+			
+			int expectedParentIndent = Convert.ToInt32(treeDoc.DocumentElement.ChildNodes[0].Attributes["Indent"].Value);
+			int actualParentIndent = Convert.ToInt32(parent.Attributes["Indent"].Value);
+			
+			Assert.AreEqual(expectedParentIndent, actualParentIndent, "Incorrect parent indent.");
+		}
+		
+			
+		/// <summary>
+		/// Tests the GetParent function where the previous node is a sibling of the new node.
+		/// </summary>		
+		[Test]
+		public void Test_GetParent_PreviousIsSibling()
+		{
+			XmlDocument treeDoc = CreateMockTreeDocument();
+			
+			XmlElement newNode = CreateMockTreeElement(treeDoc, 1);
+			
+			XmlElement previous = (XmlElement)treeDoc.DocumentElement.ChildNodes[0];
+			
+			LogGrower grower = new LogGrower(TestUtilities.GetTestDataPath(this, "MockApplication"));
+			
+			XmlElement parent = grower.GetParent(previous, newNode);
+			
+			Assert.AreEqual(treeDoc.DocumentElement, parent, "Incorrect parent returned.");
+		}
+		
+		public XmlDocument CreateMockThreadDocument()
+		{
+			XmlDocument doc = new XmlDocument();
+			
+			XmlElement logElement = doc.CreateElement("Log");
+			doc.AppendChild(logElement);
+			
+			logElement.AppendChild(CreateMockThreadElement(doc, 1));
+
+			return doc;
+		}
+		
+		
+		public XmlDocument CreateMockTreeDocument()
+		{
+			XmlDocument doc = new XmlDocument();
+			
+			XmlElement logElement = doc.CreateElement("Log");
+			doc.AppendChild(logElement);
+			
+			logElement.AppendChild(CreateMockTreeElement(doc, 1));
+
+			return doc;
+		}
+		
+		public XmlElement CreateMockThreadElement(XmlDocument doc, int indent)
+		{
+			// Entry group element
+			XmlElement entryElement = doc.CreateElement("Entry");	
+			
+			// Group ID element
+			XmlElement groupIDElement = doc.CreateElement("GroupID");
+			groupIDElement.InnerText = Guid.NewGuid().ToString();
+			entryElement.AppendChild(groupIDElement);
+			
+			// Log level element
+			XmlElement logLevelElement = doc.CreateElement("LogLevel");
+			logLevelElement.InnerText = "Debug";
+			entryElement.AppendChild(logLevelElement);
+			
+			// Time stamp element
+			XmlElement timeStampElement = doc.CreateElement("Timestamp");
+			timeStampElement.InnerText = DateTime.Now.ToString();
+			entryElement.AppendChild(timeStampElement);
+
+			// Indent element
+			XmlElement indentElement = doc.CreateElement("Indent");
+			indentElement.InnerText = indent.ToString();
+			entryElement.AppendChild(indentElement);
+
+			// Component element
+			XmlElement componentElement = doc.CreateElement("Component");
+			componentElement.InnerText = "SoftwareMonkeys.SiteStarter.MockNamespace.MockComponent";
+			entryElement.AppendChild(componentElement);
+
+			// Method element
+			XmlElement methodElement = doc.CreateElement("Method");
+			methodElement.InnerText = "MockMethod";
+			entryElement.AppendChild(methodElement);
+
+			// Data element
+			XmlElement dataElement = doc.CreateElement("Data");
+			dataElement.InnerText = "...mock data ....";
+			entryElement.AppendChild(dataElement);
+			
+			// Stack trace element
+			XmlElement stackTraceElement = doc.CreateElement("StackTrace");
+			stackTraceElement.InnerText = "...........";
+			entryElement.AppendChild(stackTraceElement);
+			
+			return entryElement;
+		}
+		
+		
+		public XmlElement CreateMockTreeElement(XmlDocument doc, int indent)
+		{
+			// Entry group element
+			XmlElement entryElement = doc.CreateElement("Entry");	
+			
+			// Group ID element
+			XmlAttribute groupIDAttribute = doc.CreateAttribute("GroupID");
+			groupIDAttribute.InnerText = Guid.NewGuid().ToString();
+			entryElement.Attributes.Append(groupIDAttribute);
+			
+			// Log level element
+			XmlAttribute logLevelAttribute = doc.CreateAttribute("LogLevel");
+			logLevelAttribute.InnerText = "Debug";
+			entryElement.Attributes.Append(logLevelAttribute);
+			
+			// Time stamp element
+			XmlAttribute timeStampAttribute = doc.CreateAttribute("Timestamp");
+			timeStampAttribute.InnerText = DateTime.Now.ToString();
+			entryElement.Attributes.Append(timeStampAttribute);
+
+			// Indent element
+			XmlAttribute indentAttribute = doc.CreateAttribute("Indent");
+			indentAttribute.InnerText = indent.ToString();
+			entryElement.Attributes.Append(indentAttribute);
+
+			// Component element
+			XmlAttribute componentAttribute = doc.CreateAttribute("Component");
+			componentAttribute.InnerText = "SoftwareMonkeys.SiteStarter.MockNamespace.MockComponent";
+			entryElement.Attributes.Append(componentAttribute);
+
+			// Method element
+			XmlAttribute methodAttribute = doc.CreateAttribute("Method");
+			methodAttribute.InnerText = "MockMethod";
+			entryElement.Attributes.Append(methodAttribute);
+
+			// Data element
+			XmlAttribute dataAttribute = doc.CreateAttribute("Data");
+			dataAttribute.InnerText = "...mock data ....";
+			entryElement.Attributes.Append(dataAttribute);
+			
+			return entryElement;
+		}
+	}
+}
---
+++ Src/App/SoftwareMonkeys.SiteStarter.Diagnostics/LogThreader.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace SoftwareMonkeys.SiteStarter.Diagnostics
+{
+	/// <summary>
+	/// Used to split logs into threads.
+	/// A thread is a section of logging output and usually has one thread per application request/operation.
+	/// </summary>
+	public class LogThreader
+	{
+		private string threadsDirectoryName = "Threads";
+		/// <summary>
+		/// Gets/sets the name of the directory containing the log threads.
+		/// </summary>
+		public string ThreadsDirectoryName
+		{
+			get { return threadsDirectoryName; }
+			set { threadsDirectoryName = value; }
+		}
+		
+		private string threadsIndexFileName = "Index.xml";
+		/// <summary>
+		/// Gets/sets the name of the file containing an index of all the threads.
+		/// </summary>
+		public string ThreadsIndexFileName
+		{
+			get { return threadsIndexFileName; }
+			set { threadsIndexFileName = value; }
+		}
+		
+		/// <summary>
+		/// Gets the full path to the threads index file.
+		/// </summary>
+		public string ThreadsIndexFilePath
+		{
+			get { return ThreadsDirectoryPath + Path.DirectorySeparatorChar + ThreadsIndexFileName; }
+		}
+			
+		/// <summary>
+		/// Gets the full path to the threads directory.
+		/// </summary>
+		public string ThreadsDirectoryPath
+		{
+			get {
+				CheckLogsDirectoryPath();
+				CheckDateStamp();
+				return LogsDirectoryPath + Path.DirectorySeparatorChar + LogDateStamp + Path.DirectorySeparatorChar + ThreadsDirectoryName; }
+		}
+		
+		
+		private string logDateStamp;
+		/// <summary>
+		/// Gets/sets the short date stamp of the log being threaded.
+		/// </summary>
+		public string LogDateStamp
+		{
+			get { return logDateStamp; }
+			set { logDateStamp = value; }
+		}		
+		
+		private string logFileName = "Log.xml";
+		/// <summary>
+		/// Gets/sets the name of the repaired log file.
+		/// </summary>
+		public string LogFileName
+		{
+			get { return logFileName; }
+			set { logFileName = value; }
+		}		
+		
+		/// <summary>
+		/// Gets the full path to the threads index file.
+		/// </summary>
+		public string LogFilePath
+		{
+			get { return LogsDirectoryPath + Path.DirectorySeparatorChar + LogDateStamp + Path.DirectorySeparatorChar + LogFileName; }
+		}
+		
+		private string repairedLogFileName = "RepairedLog.xml";
+		/// <summary>
+		/// Gets/sets the name of the repaired log file.
+		/// </summary>
+		public string RepairedLogFileName
+		{
+			get { return repairedLogFileName; }
+			set { repairedLogFileName = value; }
+		}
+		
+		
+		private string logsDirectoryPath;
+		/// <summary>
+		/// Gets/sets the full path to the directory containing the logs.
+		/// </summary>
+		public string LogsDirectoryPath
+		{
+			get { return logsDirectoryPath; }
+			set { logsDirectoryPath = value; }
+		}
+		
+		/// <summary>
+		/// Sets the logs directory path.
+		/// </summary>
+		/// <param name="logsDirectoryPath">The full path to the logs directory.</param>
+		/// <param name="logDateStamp">The short date stamp of the log being threaded.</param>
+		public LogThreader(string logsDirectoryPath, string logDateStamp)
+		{
+			LogsDirectoryPath = logsDirectoryPath;
+			LogDateStamp = logDateStamp.Replace("/", "-");
+		}
+		
+		/// <summary>
+		/// Splits the log in the specified directory into threads and places those threads into the threads directory.
+		/// </summary>
+		/// <param name="dir"></param>
+		public void SplitThreads()
+		{
+			CheckLogsDirectoryPath();
+			
+			string file = LogsDirectoryPath + Path.DirectorySeparatorChar + LogDateStamp + Path.DirectorySeparatorChar + RepairedLogFileName;
+			string indexFile = ThreadsDirectoryPath + Path.DirectorySeparatorChar + ThreadsIndexFileName;
+			
+			Guid threadID = Guid.Empty;
+			
+			RepairLog();
+			
+			if (File.Exists(file))
+			{
+				XmlDocument indexDoc = new XmlDocument();
+				indexDoc.AppendChild(indexDoc.CreateElement("Index"));
+				
+				
+				XmlDocument doc = new XmlDocument();
+				doc.Load(file);
+
+				XmlNodeList nodeList;
+				XmlNode root = doc.DocumentElement;
+				
+				if (root == null)
+					throw new Exception("Document element missing.");
+				
+				//	throw new Exception("sdf" + root.Name);
+
+				nodeList=root.SelectNodes("Entry");
+				
+				if (nodeList == null)
+					throw new Exception("No 'Entry' nodes found.");
+				
+				XmlNode threadRoot = null;
+				
+				XmlDocument threadDoc = CreateNewThread();
+				XmlDocument previousThreadDoc = null;
+				
+				
+				string threadTitle = string.Empty;
+				string threadFile = string.Empty;
+				
+				string previousThreadTitle = String.Empty;
+				
+				// OBSOLETE COMMENT: The analyses should only run if the /Detail/ directory isn't already there. If it is there then it's already been run and should be skipped.
+				if (!Directory.Exists(LogsDirectoryPath + Path.DirectorySeparatorChar + ThreadsDirectoryName))
+				{
+					Directory.CreateDirectory(LogsDirectoryPath + Path.DirectorySeparatorChar + ThreadsDirectoryName);
+				}
+				
+				//Change the price on the books.
+				foreach (XmlNode node in nodeList)
+				{
+					XmlNode indentNode = node.SelectSingleNode("Indent");
+					if (indentNode == null)
+						throw new Exception("Indent node not found.");
+					
+					
+					int indent = Convert.ToInt32(indentNode.InnerText);
+					
+					// If this is the thread root then move it to a new thread
+					if (IsThreadRoot(node))
+					{
+						// Save the previous thread
+						if (previousThreadDoc != null)
+						{
+							SaveThread(previousThreadDoc, threadID);
+							AddThreadToIndex(indexDoc, threadID, previousThreadTitle);
+						}
+						
+						threadID = Guid.NewGuid();
+						
+						// Switch the current thread to the previous one
+						previousThreadDoc = threadDoc;
+						previousThreadTitle = threadTitle;
+						
+						XmlNode componentNode = node.SelectSingleNode("Component");
+						
+						if (componentNode == null)
+							throw new Exception("No component node found.");
+						
+						XmlNode methodNode = node.SelectSingleNode("Method");
+						
+						if (methodNode == null)
+							throw new Exception("No method node found.");
+						
+						// Create the title of the new thread
+						string componentName = componentNode.InnerText;
+						string methodName = methodNode.InnerText;
+						//string nextTimeStampString = node.SelectSingleNode("Timestamp").InnerText;
+						
+						threadTitle = componentName + "." + methodName;
+						
+						threadDoc = CreateNewThread();
+						
+						
+					}
+					//else
+					//{
+					
+					//}
+					if (threadDoc == null)
+						throw new Exception("Thread doc == null");
+					
+					threadRoot = threadDoc.DocumentElement;
+					
+					if (threadRoot == null)
+						throw new Exception("Thread root node is null");
+					
+					threadRoot.AppendChild(threadDoc.ImportNode(node, true));
+					
+					//throw new Exception(indent.ToString());
+				}
+				
+				
+				if (threadDoc != null)
+				{
+					SaveThread(threadDoc, threadID);
+					AddThreadToIndex(indexDoc, threadID, threadTitle);
+				}
+				//}
+				
+				if (indexDoc == null)
+					throw new Exception("Index doc == null");
+				
+				indexDoc.Save(indexFile);
+			}
+		}
+		
+		private bool IsThreadRoot(XmlNode node)
+		{
+			XmlNode subNode = node.SelectSingleNode("Indent");
+			
+			if (subNode != null && subNode.InnerText.ToLower() == 0.ToString())
+				return true;
+			
+			return false;
+		}
+		
+		public void SaveThread(XmlDocument threadDoc, Guid threadID)
+		{
+			if (threadDoc != null)
+			{
+				string threadFile = ThreadsDirectoryPath + Path.DirectorySeparatorChar + threadID + ".xml";
+				
+				if (!Directory.Exists(Path.GetDirectoryName(threadFile)))
+					Directory.CreateDirectory(Path.GetDirectoryName(threadFile));
+				
+				threadDoc.Save(threadFile);
+			}
+		}
+		
+		public XmlDocument CreateNewThread()
+		{
+			XmlDocument threadDoc = new XmlDocument();
+			
+			XmlNode documentNode = threadDoc.CreateNode(XmlNodeType.Element, "Log", "");
+			threadDoc.AppendChild(documentNode);
+			
+			return threadDoc;
+			
+		}
+		
+		public void AddThreadToIndex(XmlDocument indexDoc, Guid threadID, string threadTitle)
+		{
+			// Add the thread to the index
+			XmlNode threadIndexNode = indexDoc.CreateElement("Thread");
+			
+			XmlAttribute idAttribute = indexDoc.CreateAttribute("ID");
+			idAttribute.Value = threadID.ToString();
+			threadIndexNode.Attributes.Append(idAttribute);
+			
+			XmlAttribute titleAttribute = indexDoc.CreateAttribute("Title");
+			titleAttribute.Value = threadTitle.ToString();
+			threadIndexNode.Attributes.Append(titleAttribute);
+			
+			indexDoc.DocumentElement.AppendChild(threadIndexNode);
+			
+		}
+		
+		public string PrepareFileName(string text)
+		{
+			
+			// first trim the raw string
+
+			string safe = text.Trim();
+
+			
+
+			// replace spaces with hyphens
+
+			safe = safe.Replace(" ", "-");
+
+			
+
+			// replace any 'double spaces' with singles
+
+			if(safe.IndexOf("--") > -1)
+
+				while(safe.IndexOf("--") > -1)
+				safe = safe.Replace("--", "-");
+			
+			// trim out illegal characters
+			safe = Regex.Replace(safe, "[^a-zA-Z0-9\\-]", "_");
+			
+			// trim the length
+			if(safe.Length > 50)
+				safe = safe.Substring(0, 49);
+			
+			// clean the beginning and end of the filename
+			char[] replace = {'-','.'};
+			safe = safe.TrimStart(replace);
+			safe = safe.TrimEnd(replace);
+
+			return safe;
+		}
+		
+		public void RepairLog()
+		{
+			CheckLogsDirectoryPath();
+			CheckDateStamp();
+			
+			string logPath = LogsDirectoryPath + Path.DirectorySeparatorChar + LogDateStamp + Path.DirectorySeparatorChar + LogFileName;
+			string fixedLogPath = LogsDirectoryPath + Path.DirectorySeparatorChar + LogDateStamp + Path.DirectorySeparatorChar + RepairedLogFileName;
+			
+			string logContents;
+			
+			using (StreamReader reader = new StreamReader(logPath))
+			{
+				logContents = reader.ReadToEnd();
+				reader.Close();
+			}
+			
+			
+			// Remove any duplicate <Log>...</Log> tags
+			logContents = new Regex("</Log>(.|\r\n)*?<Log>", RegexOptions.IgnoreCase | RegexOptions.Multiline)
+				.Replace(logContents, "");
+			
+			// Add the start <Log> tag if necessary
+			string startTag = "<Log>";
+			if (logContents.IndexOf(startTag) == -1)
+				logContents = startTag + "\r\n" + logContents;
+			
+			// Add the XML declaration if necessary
+			// This must be done before after the start tag prepend won't work
+			string xmlDeclaration = "<?xml version=\'1.0\'?>";
+			if (logContents.IndexOf(xmlDeclaration) == -1)
+				logContents = xmlDeclaration + "\r\n" + logContents;
+
+			// Add the ending </Log> tag if necessary
+			if (logContents.IndexOf("</Log>") == -1)
+				logContents = logContents + "</Log>";
+			
+			
+			using (StreamWriter writer = File.CreateText(fixedLogPath))
+			{
+				writer.Write(logContents);
+				writer.Close();
+			}
+
+		}
+		
+		public string[] GetRootParts(string dir)
+		{
+			List<string> list = new List<string>();
+			foreach (string file in Directory.GetFiles(dir))
+			{
+				list.Add(Path.GetFileName(file));
+			}
+			return list.ToArray();
+		}
+		
+		public void CheckLogsDirectoryPath()
+		{
+			if (LogsDirectoryPath == String.Empty || LogsDirectoryPath == null)
+				throw new InvalidOperationException("The LogsDirectoryPath property hasn't been set.");
+		}
+		
+		public void CheckDateStamp()
+		{
+			if (LogDateStamp == String.Empty || LogDateStamp == null)
+				throw new InvalidOperationException("The LogDateStamp property hasn't been set.");
+		}
+	}
+}
