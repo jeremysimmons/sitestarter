8e2e32666fb2c1829a3c40bdec59f1dc684ca576
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-10-17 01:19:19 +0000

    Added create strategy for creating entities Added authorisation strategies which are used within other strategies to provide security Finished index and retrieve strategies Fixed/updated strategies management mechanism

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Contracts/Business/Security/IAuthoriseSaveStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business.Security
+{
+	/// <summary>
+	/// Defines the interface of all authorise save strategies.
+	/// </summary>
+	public interface IAuthoriseSaveStrategy : IAuthoriseStrategy
+	{
+	}
+}
