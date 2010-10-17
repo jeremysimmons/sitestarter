9859f111245a4030ba10fcc955e7024fa9a4e318
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-10-17 01:19:19 +0000

    Added create strategy for creating entities Added authorisation strategies which are used within other strategies to provide security Finished index and retrieve strategies Fixed/updated strategies management mechanism

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Contracts/Business/Security/IAuthoriseIndexStrategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business.Security
+{
+	/// <summary>
+	/// Defines the interface of all authorise index strategies.
+	/// </summary>
+	public interface IAuthoriseIndexStrategy : IAuthoriseStrategy
+	{
+		/// <summary>
+		/// Checks whether the current user is authorised to index the provided entities.
+		/// </summary>
+		/// <param name="entities">The entities being indexed.</param>
+		/// <returns>A value indicating whether the current user is authorised to index the provided entities.</returns>
+		bool Authorise(ref IEntity[] entities);
+		
+		/// <summary>
+		/// Ensures that the current user is authorised to access an index of entities including the one provided.
+		/// </summary>
+		/// <param name="entities">The entities in the index.</param>
+		void EnsureAuthorised(IEntity[] entities);
+		
+		/// <summary>
+		/// Ensures that the current user is authorised to access an index of entities including the one provided.
+		/// </summary>
+		/// <param name="entities">The entities in the index.</param>
+		void EnsureAuthorised<T>(T[] entities)
+			where T : IEntity;
+	}
+}
