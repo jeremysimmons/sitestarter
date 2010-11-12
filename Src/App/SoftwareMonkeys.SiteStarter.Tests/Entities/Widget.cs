d5ef5676ca73fcdef473f16b078b4b4b2f625fcd
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-11-12 06:38:58 +0000

    Added mock Widget class

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Tests/Entities/Widget.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Configuration;
+
+namespace SoftwareMonkeys.SiteStarter.Tests.Entities
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Widget : BaseTestEntity
+	{
+		public Widget()
+		{
+		}
+		
+		/// <summary>
+		/// Registers the entity in the system.
+		/// </summary>
+		/// <param name="config">The mapping configuration object to add the settings to.</param>
+		static public void RegisterType()
+		{
+			MappingItem item = new MappingItem("Widget");
+			item.Settings.Add("DataStoreName", "Widgets");
+			item.Settings.Add("IsEntity", true);
+			item.Settings.Add("FullName", typeof(Widget).FullName);
+			item.Settings.Add("AssemblyName", typeof(Widget).Assembly.FullName);
+			
+			
+			MappingItem item2 = new MappingItem("IWidget");
+			item2.Settings.Add("Alias", "Widget");
+			
+			Config.Mappings.AddItem(item);
+			Config.Mappings.AddItem(item2);
+		}
+		
+		/// <summary>
+		/// Deregisters the entity from the system.
+		/// </summary>
+		/// <param name="config">The mapping configuration object to remove the settings from.</param>
+		static public void DeregisterType()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
