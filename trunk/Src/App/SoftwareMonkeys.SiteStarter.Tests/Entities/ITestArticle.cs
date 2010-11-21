99bcf931912ee26a2f4be9d88f66a7f951d92f14
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-11-21 09:33:39 +0000

    ITestArticle now has the AliasAttribute set to "TestArticle" ITestUser now has the AliasAttribute set to "TestUser"

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Tests/Entities/ITestArticle.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Tests/Entities/ITestArticle.cs
@@ -1,10 +1,12 @@
 using System;
+using SoftwareMonkeys.SiteStarter.Entities;
 
 namespace SoftwareMonkeys.SiteStarter.Tests.Entities
 {
 	/// <summary>
 	/// Description of ITestArticle.
 	/// </summary>
+	[Alias("TestArticle")]
 	public interface ITestArticle : SoftwareMonkeys.SiteStarter.Entities.IEntity
 	{
 	}
