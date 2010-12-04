4e102c5987987dc144a7839cb45fff7287d761fb
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-12-04 05:26:07 +0000

    Added MockControlLoader, MockUserControl, and MockProjectScanner to web tests ProjectionScannerTests fixture now uses MockControlLoader on ControlLoader property of ProjectionScanner to avoid issue with having no page object in test environment

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Web.Tests/Projections/MockProjectionScanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Web.Projections;
+
+namespace SoftwareMonkeys.SiteStarter.Web.Tests.Projections
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class MockProjectScanner : ProjectionScanner
+	{
+		public MockProjectScanner() : base(null)
+		{
+		}
+	}
+}
