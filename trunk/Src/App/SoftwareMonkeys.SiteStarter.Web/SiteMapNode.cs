9f179fc5313bd89f93a9002e08fd3d527880bf09
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-12-04 05:26:57 +0000

    Added SiteMap, SiteMapManager, and SiteMapNode

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Web/SiteMapNode.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Web.UI.WebControls;
+
+namespace SoftwareMonkeys.SiteStarter.Web
+{
+    /// <summary>
+    /// Represents a node of the site map.
+    /// </summary>
+    [Serializable]
+    public class SiteMapNode : TreeNode, IXmlSerializable, ISiteMapNode
+    {
+        private List<SiteMapNode> childNodes;
+        /// <summary>
+        /// Gets/sets the child node collection.
+        /// </summary>
+        public List<SiteMapNode> ChildNodes
+        {
+            get {
+                if (childNodes == null)
+                    childNodes = new List<SiteMapNode>();
+                return childNodes; }
+            set { childNodes = value; }
+        }
+
+        private string url = String.Empty;
+        /// <summary>
+        /// Gets/sets the URL of the page.
+        /// </summary>
+        public string Url
+        {
+        	get { return url; }
+            set { url = value; }
+        }
+
+        private string title = String.Empty;
+        /// <summary>
+        /// Gets/sets the title of the page.
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string description = String.Empty;
+        /// <summary>
+        /// Gets/sets the page description.
+        /// </summary>
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        
+        private string action = String.Empty;
+        public string Action
+        {
+        	get { return action; }
+        	set { action = value; }
+        }
+
+        private string typeName = String.Empty;
+        public string TypeName
+        {
+        	get { return typeName; }
+        	set { typeName = value; }
+        }
+        
+        private string category = String.Empty;
+        public string Category
+        {
+        	get { return category; }
+        	set { category = value; }
+        }
+        
+        public SiteMapNode()
+        {
+        	
+        }
+        
+        public SiteMapNode(string category, string title, string action, string typeName)
+        {
+        	Category = category;
+        	Title = title;
+        	Action = action;
+        	TypeName = typeName;
+        }
+        
+        #region IXmlSerializable Members
+
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+           // XmlNodeType type = reader.MoveToContent();
+           // if (type == XmlNodeType.Element && reader.LocalName.ToLower() == "sitemapnode")
+           // {
+                Title = reader["Title"];
+                Description = reader["Description"];
+                Url = reader["Url"];
+
+                while (reader.Read() && reader.Name.ToLower() == "sitemapnode")
+                {
+                    SiteMapNode node = new SiteMapNode();
+                    node.ReadXml(reader);
+                    ChildNodes.Add(node);
+                }
+            //}
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("SiteMapNode");
+
+            writer.WriteAttributeString("Title", Title);
+            writer.WriteAttributeString("Description", "Description");
+            writer.WriteAttributeString("Url", "Url");
+
+            foreach (SiteMapNode node in ChildNodes)
+            {
+                node.WriteXml(writer);
+            }
+            writer.WriteEndElement();
+        }
+
+        #endregion
+    }
+}
