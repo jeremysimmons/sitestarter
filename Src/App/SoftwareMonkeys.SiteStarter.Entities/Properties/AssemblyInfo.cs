6bb7e3fe1134bbb50dacc3416666bc0ce8696e0a
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2010-12-15 07:50:39 +0000

    Recompiled all

--- Src/App/SoftwareMonkeys.SiteStarter.Entities/Properties/AssemblyInfo.cs
+++ Src/App/SoftwareMonkeys.SiteStarter.Entities/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("2.6.4.3")]
-[assembly: AssemblyFileVersionAttribute("2.6.4.3")]
+[assembly: AssemblyVersionAttribute("2.6.4.8")]
+[assembly: AssemblyFileVersionAttribute("2.6.4.8")]
 
