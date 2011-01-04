00cec74cc743e082c864dfbfccf92563122871ce
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-01-04 10:57:21 +0000

    Added ISimple interface as a standardized way to interact with many different types of entities.

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Contracts/Entities/ISimple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SoftwareMonkeys.SiteStarter.Entities
+{
+	/// <summary>
+	/// Defines the simple interface adopted by most entities.
+	/// </summary>
+	public interface ISimple : IEntity
+	{
+		string Title {get;set;}
+		string Description {get;set;}
+		
+	}
+}
