b7608220b354ac9ab2b2499148e96bc5573e4e33
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-01-06 02:23:11 +0000

    Fixed IndexControllerTests

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Web.Tests/Properties/AssemblyInfo.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Web.Tests/Properties/AssemblyInfo.cs
@@ -16,10 +16,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfigurationAttribute("")]
 [assembly: AssemblyCompanyAttribute("SoftwareMonkeys")]
 [assembly: AssemblyProductAttribute("SiteStarter.Web.Tests")]
-[assembly: AssemblyCopyrightAttribute("Copyright SoftwareMonkeys (c) 2010")]
+[assembly: AssemblyCopyrightAttribute("Copyright SoftwareMonkeys (c) 2011")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("2.6.5.3")]
-[assembly: AssemblyFileVersionAttribute("2.6.5.3")]
+[assembly: AssemblyVersionAttribute("2.6.7.2")]
+[assembly: AssemblyFileVersionAttribute("2.6.7.2")]
 
