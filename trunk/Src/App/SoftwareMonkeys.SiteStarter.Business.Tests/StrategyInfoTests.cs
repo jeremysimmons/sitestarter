3b0124f8aed78bf9db781a661b05eeaa9f19ffa8
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-01-15 03:55:25 +0000

    Added StrategyInfoTests to ensure that the constructor does its job properly

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/StrategyInfoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SoftwareMonkeys.SiteStarter.Business.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class StrategyInfoTests : BaseBusinessTestFixture
+	{
+		[Test]
+		public void Test_Constructor_StrategyParameter()
+		{
+			MockIndexWidgetStrategy strategy = new MockIndexWidgetStrategy();
+			
+			StrategyInfo info = new StrategyInfo(strategy);
+			
+			Assert.AreEqual("Widget", info.TypeName, "Type name doesn't match what's expected.");
+			Assert.AreEqual("Index", info.Action, "Action doesn't match what's expected.");
+			//Assert.AreEqual("Index_Widget", info.Key, "Key doesn't match what's expected.");
+			Assert.AreEqual(strategy.GetType().FullName + ", " + strategy.GetType().Assembly.GetName().Name, info.StrategyType, "Strategy type doesn't match what's expected.");
+		}
+	}
+}
