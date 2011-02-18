ed31c8248468bb5ec77a61b6aa49719dceb7b1cd
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-02-18 23:53:50 +0000

    Fixed DeleteSubEntityStrategyTests

--- Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/DeleteSubEntityStrategyTests.cs
+++ Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/DeleteSubEntityStrategyTests.cs
@@ -24,6 +24,7 @@ namespace SoftwareMonkeys.SiteStarter.Business.Tests
 			
 			Collection<TestArticlePage> pages = new Collection<TestArticlePage>();
 			
+			// Create and save 3 article pages associated with the article
 			for (int i = 0; i < 3; i++)
 			{
 				TestArticlePage page = new TestArticlePage();
@@ -37,15 +38,20 @@ namespace SoftwareMonkeys.SiteStarter.Business.Tests
 				SaveStrategy.New(page, false).Save(page);
 			}
 			
+			// Delete the second page (0 based position = 1)
 			DeleteStrategy.New(pages[1], false).Delete(pages[1]);
 			
+			// Load the article from the store
 			TestArticle foundArticle = RetrieveStrategy.New<TestArticle>(false).Retrieve<TestArticle>("ID", article.ID);
 			
+			// Activate the pages on the article
 			ActivateStrategy.New(foundArticle, false).Activate(foundArticle, "Pages");
 			
 			Assert.IsNotNull(foundArticle.Pages, "Pages property isn't set.");
 			
 			Assert.AreEqual(2, foundArticle.Pages.Length, "Invalid number of pages.");
+			
+			foundArticle.Pages = Collection<TestArticlePage>.Sort(foundArticle.Pages, "PageNumberAscending");
 			
 			Assert.AreEqual(1, foundArticle.Pages[0].PageNumber, "First page has wrong number.");
 			Assert.AreEqual("Page 1", foundArticle.Pages[0].Title, "First page has wrong title.");
