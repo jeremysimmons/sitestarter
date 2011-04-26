b127e4e5c9c320f9e9da2583d2550cf3e0441292
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-04-26 02:48:29 +0000

    Added business reactions framework from SiteStarterLab

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business/ReactionAttribute.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Description of ReactionAttribute.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple=false)]
+	public class ReactionAttribute : Attribute
+	{
+		private string typeName;
+		/// <summary>
+		/// Gets/sets the name of the type involved in the reaction.
+		/// </summary>
+		public string TypeName
+		{
+			get { return typeName; }
+			set { typeName = value; }
+		}
+		
+		private string action;
+		/// <summary>
+		/// Gets/sets the action that can be performed by this reaction.
+		/// </summary>
+		public string Action
+		{
+			get { return action; }
+			set { action = value; }
+		}
+		
+		private bool isReaction = true;
+		/// <summary>
+		/// Gets/sets a value indicating whether the corresponding class is an available reaction. (Set to false to hide a reaction or a base class.)
+		/// </summary>
+		public bool IsReaction
+		{
+			get { return isReaction; }
+			set { isReaction = value; }
+		}
+		
+		/// <summary>
+		/// Sets the type name and action of the reaction.
+		/// </summary>
+		/// <param name="action"></param>
+		/// <param name="typeName"></param>
+		public ReactionAttribute(string action, string typeName)
+		{
+			TypeName = typeName;
+			Action = action;
+		}
+		
+		/// <summary>
+		/// Sets the type name and action of the reaction.
+		/// </summary>
+		/// <param name="action"></param>
+		/// <param name="type"></param>
+		public ReactionAttribute(string action, Type type)
+		{
+			TypeName = type.Name;
+			Action = action;
+		}
+		
+		/// <summary>
+		/// Sets a value indicating whether the corresponding class is an available business reaction.
+		/// </summary>
+		/// <param name="isReaction">A value indicating whether the corresponding class is an available business reaction. (Set to false for base reactions, etc.)</param>
+		public ReactionAttribute(bool isReaction)
+		{
+			IsReaction = isReaction;
+		}
+	}
+}
---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business/ReactionInitializer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Data;
+using System.IO;
+using SoftwareMonkeys.SiteStarter.Diagnostics;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Used to initialize the reaction state and make reactions available for use.
+	/// </summary>
+	public class ReactionInitializer
+	{
+		private ReactionFileNamer fileNamer;
+		/// <summary>
+		/// Gets/sets the file namer used to create reaction file names/paths.
+		/// </summary>
+		public ReactionFileNamer FileNamer
+		{
+			get {
+				if (fileNamer == null)
+					fileNamer = new ReactionFileNamer();
+				return fileNamer; }
+			set { fileNamer = value; }
+		}
+		
+		private ReactionSaver saver;
+		/// <summary>
+		/// Gets/sets the reaction saver used to save reactions to file.
+		/// </summary>
+		public ReactionSaver Saver
+		{
+			get {
+				if (saver == null)
+				{
+					saver = new ReactionSaver();
+					if (ReactionsDirectoryPath != null && ReactionsDirectoryPath != String.Empty)
+						saver.ReactionsDirectoryPath = ReactionsDirectoryPath;
+				}
+				return saver; }
+			set { saver = value; }
+		}
+		
+		private ReactionScanner scanner;
+		/// <summary>
+		/// Gets/sets the reaction scanner used to find available reactions in the existing assemblies.
+		/// </summary>
+		public ReactionScanner Scanner
+		{
+			get {
+				if (scanner == null)
+				{
+					scanner = new ReactionScanner();
+				}
+				return scanner; }
+			set { scanner = value; }
+		}
+		
+		private ReactionLoader loader;
+		/// <summary>
+		/// Gets/sets the reaction loader used to find available reactions in the existing assemblies.
+		/// </summary>
+		public ReactionLoader Loader
+		{
+			get {
+				if (loader == null)
+				{
+					loader = new ReactionLoader();
+					if (ReactionsDirectoryPath != null && ReactionsDirectoryPath != String.Empty)
+						loader.ReactionsDirectoryPath = ReactionsDirectoryPath;
+				}
+				return loader; }
+			set { loader = value; }
+		}
+		
+		/// <summary>
+		/// Gets a value indicating whether the reactions have been mapped yet.
+		/// </summary>
+		public bool IsMapped
+		{
+			get {
+				bool isMapped = ReactionMappingsExist();
+				return isMapped; }
+		}
+		
+		/// <summary>
+		/// Gets the full path to the directory containing reaction mappings.
+		/// </summary>
+		public string ReactionsDirectoryPath
+		{
+			get { return FileNamer.ReactionsDirectoryPath; }
+		}
+		
+		public ReactionInitializer()
+		{
+		}
+		
+		
+		/// <summary>
+		/// Initializes the reactions and loads all reactions to state.
+		/// </summary>
+		/// <param name="reactions">The reactions to initialize.</param>
+		public void Initialize(ReactionInfo[] reactions)
+		{
+			ReactionState.Reactions = new ReactionStateNameValueCollection(reactions);
+		}
+		
+		/// <summary>
+		/// Initializes the reactions and loads all reactions to state.
+		/// </summary>
+		public void Initialize()
+		{
+			using (LogGroup logGroup = LogGroup.Start("Initializing the business reactions.", NLog.LogLevel.Debug))
+			{
+				ReactionInfo[] reactions = new ReactionInfo[]{};
+				if (IsMapped)
+				{
+					LogWriter.Debug("Is mapped. Loading from XML.");
+					
+					reactions = LoadReactions();
+				}
+				else
+				{
+					LogWriter.Debug("Is not mapped. Scanning from type attributes.");
+					
+					reactions = FindReactions();
+					SaveToFile(reactions);
+				}
+				
+				Initialize(reactions);
+			}
+		}
+		
+		/// <summary>
+		/// Saves the mappings for the provided reactions to reactions mappings directory.
+		/// </summary>
+		/// <param name="reactions">The reactions to save to file.</param>
+		public void SaveToFile(ReactionInfo[] reactions)
+		{
+			using (LogGroup logGroup = LogGroup.Start("Saving the provided reactions to XML.", NLog.LogLevel.Debug))
+			{
+				foreach (ReactionInfo reaction in reactions)
+				{
+					Saver.SaveToFile(reaction);
+				}
+			}
+		}
+		
+		
+		/// <summary>
+		/// Loads the available reactions from file.
+		/// </summary>
+		/// <returns>The loaded from the reactions mappings directory.</returns>
+		public ReactionInfo[] LoadReactions()
+		{
+			return Loader.LoadFromDirectory();
+		}
+		
+		/// <summary>
+		/// Finds all the reactions available to the application.
+		/// </summary>
+		/// <returns>An array of the available reactions.</returns>
+		public ReactionInfo[] FindReactions()
+		{
+			return Scanner.FindReactions();
+		}
+		
+		/// <summary>
+		/// Checks whether the reaction mappings have been created and saved to file.
+		/// </summary>
+		/// <returns>A value indicating whether the reaction mappings directory was found.</returns>
+		public bool ReactionMappingsExist()
+		{
+			string directory = ReactionsDirectoryPath;
+			
+			return (Directory.Exists(directory) && Directory.GetFiles(directory).Length > 0);
+		}
+	}
+	
+}
