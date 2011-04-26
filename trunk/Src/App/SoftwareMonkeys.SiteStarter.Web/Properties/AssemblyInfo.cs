11c013f5a984431c6d93ca6d02200b7e1e2a7aff
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-04-26 03:26:58 +0000

    Now version 2.6.8.*

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Web/Properties/AssemblyInfo.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Web/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("2.6.7.62")]
-[assembly: AssemblyFileVersionAttribute("2.6.7.62")]
+[assembly: AssemblyVersionAttribute("2.6.7.77")]
+[assembly: AssemblyFileVersionAttribute("2.6.7.77")]
 
