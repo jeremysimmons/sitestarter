d7d01deccdfebf1fe976916568745ad156c5b377
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-04-27 01:08:22 +0000

    Added missing ISaveReaction

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Contracts/Business/ISaveReaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Defines the interface of all save reactions.
+	/// </summary>
+	public interface ISaveReaction : IReaction
+	{
+		/// <summary>
+		/// Reacts to a save action.
+		/// </summary>
+		/// <param name="entity">The entity that was saved.</param>
+		void React(IEntity entity);
+	}
+}
