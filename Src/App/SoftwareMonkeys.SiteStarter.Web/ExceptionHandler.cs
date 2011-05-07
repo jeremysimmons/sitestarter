d1cff251eae8c290e11884fede2c84cf3410bae8
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-05-07 10:58:05 +0000

    Tweaked exception handling to boost stability

--- Src/App/SoftwareMonkeys.SiteStarter.Web/ExceptionHandler.cs
+++ Src/App/SoftwareMonkeys.SiteStarter.Web/ExceptionHandler.cs
@@ -19,56 +19,69 @@ namespace SoftwareMonkeys.SiteStarter.Web
 		
 		public void Handle(Exception exception)
 		{
-			try
+			if (exception != null)
 			{
-				LogWriter.Error(exception);
-				
-				SendEmail(exception);
-				
-				if (Convert.ToBoolean(ConfigurationSettings.AppSettings["EnableErrorHandling"]))
+				try
 				{
-					LogWriter.Debug("Error handling is enabled. Redirecting to error page.");
+					LogWriter.Error(exception);
 					
-					// Send the user to the error page if they aren't already there
-					if (HttpContext.Current.Request.Url.ToString().ToLower().IndexOf("error.aspx") == -1)
-						HttpContext.Current.Server.Transfer(HttpContext.Current.Request.ApplicationPath.TrimEnd('/') + "/Error.aspx");
+					SendEmail(exception);
+					
+					if (Convert.ToBoolean(ConfigurationSettings.AppSettings["EnableErrorHandling"]))
+					{
+						LogWriter.Debug("Error handling is enabled. Redirecting to error page.");
+						
+						// Send the user to the error page if they aren't already there
+						if (HttpContext.Current.Request.Url.ToString().ToLower().IndexOf("error.aspx") == -1)
+							HttpContext.Current.Server.Transfer(HttpContext.Current.Request.ApplicationPath.TrimEnd('/') + "/Error.aspx");
+					}
+					else
+						LogWriter.Debug("Error handling is disabled");
 				}
-				else
-					LogWriter.Debug("Error handling is disabled");
-			}
-			catch (Exception ex)
-			{
-				using (LogGroup logGroup = LogGroup.StartDebug("Error handling failed. There was an error within the error handling itself."))
+				catch (Exception ex)
 				{
-					LogWriter.Error(ex);
-					throw ex;
+					using (LogGroup logGroup = LogGroup.StartDebug("Error handling failed. There was an error within the error handling itself."))
+					{
+						LogWriter.Error(ex);
+						throw ex;
+					}
 				}
 			}
 		}
 		
 		public void SendEmail(Exception exception)
 		{
-			if (new ModeDetector().IsRelease)
+			try
 			{
-			UserRole role = RetrieveStrategy.New<UserRole>().Retrieve<UserRole>("Name", "Administrator");
-			
-			ActivateStrategy.New(role).Activate(role, "Users");
-			
-			foreach (User user in role.Users)
-			{
-				string subject = "Exception";
-				string message = "An exception occurred...\n"
-					+ "URL:\n"
-					+ HttpContext.Current.Request.Url.ToString() + "\n"
-					+ "Site:\n"
-					+ WebUtilities.ConvertRelativeUrlToAbsoluteUrl(HttpContext.Current.Request.ApplicationPath) + "\n"
-					+ "Time:\n"
-					+ DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString() + "\n"
-					+ "Exception:\n"
-					+ exception.ToString();
-				
-				Emailer.New().SendEmail(user, subject, message);
+				if (new ModeDetector().IsRelease)
+				{
+					UserRole role = RetrieveStrategy.New<UserRole>(false).Retrieve<UserRole>("Name", "Administrator");
+					
+					ActivateStrategy.New(role, false).Activate(role, "Users");
+					
+					foreach (User user in role.Users)
+					{
+						string subject = "Exception";
+						string message = "An exception occurred...\n"
+							+ "URL:\n"
+							+ HttpContext.Current.Request.Url.ToString() + "\n\n"
+							+ "Site:\n"
+							+ WebUtilities.ConvertRelativeUrlToAbsoluteUrl(HttpContext.Current.Request.ApplicationPath) + "\n\n"
+							+ "Time:\n"
+							+ DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString() + "\n\n"
+							+ "Exception:\n"
+							+ exception.ToString() + "\n\n"
+							+ "(please do not reply to this email)\n";
+						
+						Emailer.New().SendEmail(user, subject, message);
+					}
+				}
 			}
+			catch(Exception ex)
+			{
+				LogWriter.Error("Exception occurred when trying to send error report email.");
+				
+				LogWriter.Error(ex);
 			}
 		}
 	}
