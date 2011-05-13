b188660cffe9f9622354a6178f3ed1b225e28825
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-05-13 05:26:34 +0000

    Imported latest integration tests from ProjectCommander

--- Src/App/IntegrationTests/Run.bat
+++ Src/App/IntegrationTests/Run.bat
@@ -1,3 +1,25 @@
+@echo off
+
+set REL_PATH=\SiteStarter.Index.html
+set ABS_SUITEPATH=
+
+rem // save current directory
+pushd .
+
+rem // change to relative directory and save value of CD (current directory) variable
+cd %REL_PATH%
+set ABS_SUITEPATH=%CD%
+
+rem // restore current directory
+popd
+
+echo Relative path : %REL_PATH%
+echo Maps to path  : %ABS_SUITEPATH%
+
 mkdir "Results"
-java -jar ../../../lib/selenium-server.jar -htmlSuite "*firefox" "http://localhost/SiteStarter/" "SiteStarter.Index.html" "Results/Results.html"
+
+set ABS_SUITEPATH=%ABS_SUITEPATH%%REL_PATH%
+
+java -jar ../../../lib/selenium-server.jar -htmlSuite "*iexplore" "http://localhost/SiteStarter/" "%ABS_SUITEPATH%" "Results/Results.html"
+
 pause
