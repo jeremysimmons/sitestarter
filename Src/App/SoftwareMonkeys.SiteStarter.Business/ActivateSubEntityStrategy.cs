20313bd91821af008cd5042aa9ed67c3bceb652f
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-05-18 06:28:51 +0000

    Added ActivateSubEntityStrategy.cs to fix issue with activate strategy now being found for "ISubEntity" - doesnt contain any implementation

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business/ActivateSubEntityStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Strategy("Activate", "ISubEntity")]
+	public class ActiveSubEntityStrategy : ActivateStrategy
+	{
+		public ActiveSubEntityStrategy()
+		{
+		}
+	}
+}
