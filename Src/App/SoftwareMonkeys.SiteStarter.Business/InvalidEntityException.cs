66a9f14e5a7096418f0d3d64e74a0cdc1c9b3353
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-06-02 12:25:09 +0000

    Added InvalidEntityException - thrown when an invalid entity is being saved or updated

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business/InvalidEntityException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Thrown when attempting an operation which requires entity.IsValid to be true but is currently false.
+	/// </summary>
+	public class InvalidEntityException : Exception
+	{
+		private IEntity entity;
+		public IEntity Entity
+		{
+			get { return entity; }
+			set { entity = value; }
+		}
+		
+		public InvalidEntityException(IEntity entity) : base("The '" + entity.ShortTypeName + "' entity has not been validated.")
+		{
+			Entity = entity;
+		}
+	}
+}
