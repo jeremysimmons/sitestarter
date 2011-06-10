b8e09a0dff465551854a26ebb1b51afe34098c46
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-06-10 11:33:11 +0000

    Added AssemblyContainsEntitiesAttribute which indicates that an assembly contains entity classes EntityScanner now only scans assemblies marked with the AssemblyContainsEntitiesAttribute Added AssemblyScanInfo to SoftwareMonkeys.SiteStarter.Entities assembly containing an AssemblyContainsControllersAttribute

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Entities.Tests/EntityScannerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using SoftwareMonkeys.SiteStarter.Tests;
+
+namespace SoftwareMonkeys.SiteStarter.Entities.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class EntityScannerTests : BaseTestFixture
+	{
+		public EntityScannerTests()
+		{
+		}
+		
+		[Test]
+		public void Test_ContainsEntities_MatchingAssembly()
+		{
+			Assembly matchingAssembly = Assembly.Load("SoftwareMonkeys.SiteStarter.Entities");
+			
+			EntityScanner scanner = new EntityScanner();
+			
+			bool doesMatch = scanner.ContainsEntities(matchingAssembly);
+			
+			Assert.IsTrue(doesMatch, "Failed to match when it should.");
+		}
+		
+		[Test]
+		public void Test_ContainsEntities_NonMatchingAssembly()
+		{
+			Assembly assembly = Assembly.Load("SoftwareMonkeys.SiteStarter.Business");
+			
+			EntityScanner scanner = new EntityScanner();
+			
+			bool doesMatch = scanner.ContainsEntities(assembly);
+			
+			Assert.IsFalse(doesMatch, "Matched when it shouldn't have.");
+		}
+	}
+}
---
+++ Src/App/SoftwareMonkeys.SiteStarter.Entities/AssemblyContainsEntitiesAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoftwareMonkeys.SiteStarter.Entities
+{
+	/// <summary>
+	/// Description of AssemblyContainsEntitiesAttribute.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class AssemblyContainsEntitiesAttribute : Attribute
+	{
+		public AssemblyContainsEntitiesAttribute()
+		{
+		}
+	}
+}
