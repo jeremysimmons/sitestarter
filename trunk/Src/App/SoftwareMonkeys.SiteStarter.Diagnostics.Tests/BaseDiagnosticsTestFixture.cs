40de6787fba3564ca7dc8db4502ec6278c40c26a
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-06-22 10:23:16 +0000

    Added/updated diagnostics tests to ensure that the overhauled logging system still works properly

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Diagnostics.Tests/BaseDiagnosticsTestFixture.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Diagnostics.Tests/BaseDiagnosticsTestFixture.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using NUnit.Framework;
 using SoftwareMonkeys.SiteStarter.Tests;
+using SoftwareMonkeys.SiteStarter.State;
 using SoftwareMonkeys.SiteStarter.State.Tests;
+using SoftwareMonkeys.SiteStarter.Diagnostics;
+using System.Reflection;
+using System.IO;
+using NLog;
 
 namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
 {
@@ -10,10 +16,14 @@ namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
 	/// </summary>
 	public class BaseDiagnosticsTestFixture : BaseStateTestFixture
 	{
+		public LogGroup FixtureLogGroup = null;
+		public bool EnableTestLogging = true;
+		
 		[SetUp]
-		public void Start()
+		public override void Start()
 		{
-			TestUtilities.ClearTestingDirectory(this);
+			base.Start();
+			
 			InitializeMockDiagnostics();
 		}
 		
@@ -21,10 +31,12 @@ namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
 		/// Ends a test by disposing the mock test environment and deleting mock data.
 		/// </summary>
 		[TearDown]
-		public void End()
+		public override void End()
 		{
 			DisposeMockDiagnostics();
-			TestUtilities.ClearTestingDirectory(this);
+			
+			base.End();
+			
 		}
 		
 		
@@ -33,7 +45,14 @@ namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
 		/// </summary>
 		public virtual void InitializeMockDiagnostics()
 		{
-			
+			if (EnableTestLogging)
+			{
+				// Create a log group for the test
+				FixtureLogGroup = LogGroup.Start("Starting test '" + TestName + "'.", LogLevel.Info);
+				
+				// Enable debug logging during testing
+				LogSettingsManager.Current.Defaults["Debug"] = new ModeDetector().IsDebug;
+			}
 		}
 		
 		/// <summary>
@@ -41,7 +60,94 @@ namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
 		/// </summary>
 		public virtual void DisposeMockDiagnostics()
 		{
+			if (EnableTestLogging)
+			{
+				FixtureLogGroup.Dispose();
+				FixtureLogGroup = null;
+			}
 			
+			DiagnosticState.Dispose();
+			
+			ReportLogs();
+		}
+		
+		public void ReportLogs()
+		{
+			string reportDirectory = GetLogReportDirectory();
+			
+			// TODO: Clean up
+			//if (StateAccess.IsInitialized)
+			//{
+			new LogReporter(reportDirectory).Report(GetType().Name + Path.DirectorySeparatorChar + TestName);
+			//}
+		}
+		
+		public string GetLogReportDirectory()
+		{
+			string cd = Environment.CurrentDirectory;
+			
+			string reportDirectory = String.Empty;
+			
+			string version = Assembly.GetExecutingAssembly().GetName().Version.ToString().Replace(".", "-");
+			
+			string subDir = "TestResults/Logs/" + version;
+			
+			string stepUp = "/";
+			
+			// TODO: Clean up
+			// If the current directory is within an assembly bin directory
+			//if (cd.IndexOf(GetType().Assembly.GetName().Name.Replace(".", "-")) > -1)
+			
+			// If the current directory is within the /src/ directory
+			if (cd.ToLower().IndexOf(Path.DirectorySeparatorChar + "src" + Path.DirectorySeparatorChar) > -1)
+			{
+				stepUp = "/../../../../../"; // Removing /Src/App/[namespace]/bin/[mode]/
+			}
+			// Otherwise it's in the general bin directory
+			else
+			{
+				stepUp = "/../../"; // Removing /bin/[mode]/
+			}
+			
+			reportDirectory = Path.GetFullPath(cd + stepUp + subDir);
+			
+			// Add the namespace to the path
+			reportDirectory = reportDirectory + Path.DirectorySeparatorChar + ShortenNamespace(GetType().Namespace).Replace(".", "-");
+			
+			return reportDirectory;
+		}
+		
+		/// <summary>
+		/// Shortens the provided namespace by removing the first (company) and second (project) parts.
+		/// </summary>
+		/// <param name="original"></param>
+		/// <returns></returns>
+		public string ShortenNamespace(string original)
+		{
+			string output = original;
+			
+			string[] parts = original.Split('.');
+			
+			// If the namespace has more than 2 parts remove the first two (company and project)
+			if (parts.Length > 2)
+				output = original.Replace(parts[0] + "." // Company name
+				                          + parts[1] + ".", // Project name
+				                          "");
+			
+			return output;
+		}
+		
+		protected string LoadLogContent(string logPath)
+		{
+			string content = String.Empty;
+			if (File.Exists(logPath))
+			{
+				using (StreamReader reader = new StreamReader(File.OpenRead(logPath)))
+				{
+					content = reader.ReadToEnd();
+				}
+			}
+			return content;
 		}
 	}
 }
---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Diagnostics.Tests/MockLogSettingsManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SoftwareMonkeys.SiteStarter.Diagnostics.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class MockLogSettingsManager : LogSettingsManager
+	{
+		public bool Enable = false;
+		public bool Specified = false;
+		
+		static private LogSettingsManager testManager;
+		static public LogSettingsManager TestManager
+		{
+			get {
+				if (testManager == null)
+					testManager = MockLogSettingsManager.NewSpecified(true);
+				return testManager; }
+			set { testManager = value; }
+		}
+		
+		public MockLogSettingsManager()
+		{
+		
+		}
+		
+		public override bool IsEnabled(LogLevel level)
+		{
+			return Enable;
+		}
+		
+		public override bool IsEnabled(LogLevel level, bool defaultValue)
+		{
+			if (Specified)
+				return Enable;
+			else
+				return defaultValue;
+		}
+		           
+		public override bool IsEnabled(string key, LogLevel level)
+		{
+			return Enable;
+		}
+		
+		public override bool IsEnabled(string key, LogLevel level, bool defaultValue)
+		{
+			if (Specified)
+				return Enable;
+			else
+				return IsEnabled(level, defaultValue);
+		}
+		
+		static public MockLogSettingsManager NewSpecified(bool enable)
+		{
+			MockLogSettingsManager manager = new MockLogSettingsManager();
+			manager.Specified = true;
+			manager.Enable = enable;
+			return manager;
+		}
+		
+		static public MockLogSettingsManager NewNotSpecified()
+		{
+			MockLogSettingsManager manager = new MockLogSettingsManager();
+			manager.Specified = false;
+			return manager;
+		}
+	}
+}
