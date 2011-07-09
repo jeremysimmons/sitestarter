7092f355a56439ff5adc2ca547ac4dd5ea7fb6bb
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-07-09 12:58:39 +0000

    Regenerated C# versions of Update.html functional test

--- Src/App/SoftwareMonkeys.SiteStarter.Functional.firefox.Tests/UpdateTestFixture_firefox.cs
+++ Src/App/SoftwareMonkeys.SiteStarter.Functional.firefox.Tests/UpdateTestFixture_firefox.cs
@@ -96,31 +96,6 @@ namespace SoftwareMonkeys.SiteStarter.Functional.firefox.Tests
 			selenium.Click("link=Roles");
 			selenium.WaitForPageToLoad("30000");
 			Assert.IsTrue(selenium.IsTextPresent("another role"), "Text 'another role' not found when it should be.");
-			selenium.Click("link=Sign Out");
-			selenium.WaitForPageToLoad("30000");
-			selenium.Type("ctl00_Body_ctl00_Login_UserName", "admin");
-			selenium.Type("ctl00_Body_ctl00_Login_Password", "pass");
-			selenium.Click("ctl00_Body_ctl00_Login_LoginButton");
-			selenium.WaitForPageToLoad("30000");
-			selenium.Open("Admin/Data.aspx");
-			selenium.Click("link=User-UserRole");
-			selenium.WaitForPageToLoad("30000");
-			Assert.IsTrue(selenium.IsTextPresent("System Administrator"), "Text 'System Administrator' not found when it should be.");
-			selenium.Click("UpdateApplicationLink");
-			selenium.WaitForPageToLoad("30000");
-			selenium.WaitForPageToLoad("30000");
-			Assert.IsTrue(selenium.IsTextPresent("Application Update"), "Text 'Application Update' not found when it should be.");
-			Assert.IsTrue(selenium.IsTextPresent("current application version"), "Text 'current application version' not found when it should be.");
-			selenium.Click("//input[@value='Begin »']");
-			selenium.WaitForPageToLoad("30000");
-			selenium.WaitForPageToLoad("30000");
-			Assert.IsTrue(selenium.IsTextPresent("Ready for Upload"), "Text 'Ready for Upload' not found when it should be.");
-			Assert.IsTrue(selenium.IsTextPresent("Your data has been backed up"), "Text 'Your data has been backed up' not found when it should be.");
-			selenium.Click("//input[@value='Continue »']");
-			selenium.WaitForPageToLoad("30000");
-			selenium.WaitForPageToLoad("30000");
-			Assert.IsTrue(selenium.IsTextPresent("Update Complete"), "Text 'Update Complete' not found when it should be.");
-			Assert.IsTrue(selenium.IsTextPresent("successfully"), "Text 'successfully' not found when it should be.");
 
 		}
 	}
