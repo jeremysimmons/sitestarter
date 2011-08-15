3830d09d5271dc7710fb7da399a20e799492e8f4
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-08-15 23:33:48 +0000

    Javascript errors are now logged

---
+++ trunk/Src/App/WWW/js/errors.js
@@ -0,0 +1,21 @@
+﻿ var applicationPath = '';
+ 
+ function handleError(msg, srcUrl, line){
+ 
+       var receiveReq = createHttpRequest();
+					
+		var url = applicationPath + '/Admin/LogJavascriptError.aspx';
+		
+		var data  = "ErrorData=" + msg + "&SourcePage=" + srcUrl + "&Line=" + line;
+		
+		receiveReq.open("POST", url, true);
+						 
+		receiveReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		receiveReq.setRequestHeader("Content-length", data.length);
+		receiveReq.setRequestHeader("Connection", "close");
+							 
+		receiveReq.send(data);
+}
+
+window.onerror=handleError;
+ 
