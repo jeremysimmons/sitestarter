5b85aacf11ef95f1447527cdaf905c12f7b32281
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-10-13 09:48:03 +0000

    Added AssemblyContainsElementsAttribute to ...Web assembly

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Web/AssemblyScanInfo.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Web/AssemblyScanInfo.cs
@@ -1,3 +1,7 @@
 ﻿using SoftwareMonkeys.SiteStarter.Web.Controllers;
+using SoftwareMonkeys.SiteStarter.Web.Elements;
 
+[assembly: AssemblyContainsElements()]
 [assembly: AssemblyContainsControllers()]
+
+// This file doesn't need attributes for parts and projections because they're ascx files and not in the assembly
