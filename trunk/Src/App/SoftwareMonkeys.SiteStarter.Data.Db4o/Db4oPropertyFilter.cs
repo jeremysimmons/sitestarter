76d160ca69f6803e17f019fd13447f47a0dae7c6
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-10-15 10:37:04 +0000

    Removed obsolete code from Db4oPropertyFilter

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Data.Db4o/Db4oPropertyFilter.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Data.Db4o/Db4oPropertyFilter.cs
@@ -15,44 +15,7 @@ namespace SoftwareMonkeys.SiteStarter.Data.Db4o
     /// </summary>
     public class Db4oPropertyFilter : PropertyFilter
     {
-    	// TODO: Check if needed
-    	// The base implementation should take care of it
-        /*public override bool IsMatch(IEntity entity)
-        {
-            bool isMatch = false;
-
-            //using (LogGroup logGroup2 = LogGroup.Start("Checking whether the filter matches the provided entity.", NLog.LogLevel.Debug))
-            //{
-            //    LogWriter.Debug("Field Name: " + PropertyName);
-            //    LogWriter.Debug("Field Value: " + PropertyValue);
-
-                Type type = entity.GetType();
-
-            //    LogWriter.Debug("Entity type: " + type.ToString());
-
-                if (Types == null)
-                {
-            //        LogWriter.Debug("No filter types have been specified. Match failed.");
-                }
-                else if (Array.IndexOf(Types, type) == -1)
-                {
-            //        LogWriter.Debug("The provided entity type isn't allowed by the filter. Match failed.");
-                }
-                else
-                {
-                    PropertyInfo property = type.GetProperty(PropertyName);
-
-            //        LogWriter.Debug("Property name: " + property.Name);
-
-                    isMatch = property.GetValue(entity, null).Equals(PropertyValue);
-                }
-
-
-            //    LogWriter.Debug("Is match? " + isMatch.ToString());
-            //}
-
-            return isMatch;
-        }*/
+    	// Use the base implementation
     }
 
 }
