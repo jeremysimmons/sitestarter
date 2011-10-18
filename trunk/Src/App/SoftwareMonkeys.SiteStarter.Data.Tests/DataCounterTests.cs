4689dfc2a29005a69edd61860458b7015591f416
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-10-18 10:48:04 +0000

    Added data counter for counting entities without actually loading them (where possible)

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Data.Tests/DataCounterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using SoftwareMonkeys.SiteStarter.Tests.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Data.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class DataCounterTests : BaseDataTestFixture
+	{
+		public DataCounterTests()
+		{
+		}
+		
+		[Test]
+		public void Test_CountEntities_Generic_NoParameters()
+		{
+			TestRecord record = new TestRecord();
+			record.ID = Guid.NewGuid();
+			record.Name = "Test";
+			
+			int before = DataAccess.Data.Counter.CountEntities<TestRecord>();
+			
+			Assert.AreEqual(0, before);
+			
+			DataAccess.Data.Saver.Save(record);
+			
+			int after = DataAccess.Data.Counter.CountEntities<TestRecord>();
+			
+			Assert.AreEqual(1, after);
+		}
+	}
+}
---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Data/IDataCounter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+using System.Collections.Generic;
+
+namespace SoftwareMonkeys.SiteStarter.Data
+{
+	/// <summary>
+	/// Defines the interface for data indexer adapters.
+	/// </summary>
+	public interface IDataCounter : IDataAdapter
+	{
+		int CountEntities<T>()
+			where T : IEntity;
+				
+		int CountEntities<T>(string propertyName, object value)
+			where T : IEntity;
+		
+		int CountEntities(Type type, Guid[] ids);
+
+		int CountEntities<T>(Guid[] ids)
+			where T : IEntity;
+
+
+        int CountEntities();
+
+		int CountEntities(Type type);
+
+		int CountEntities(Type type, Dictionary<string, object> parameters);
+
+		/// <summary>
+		/// Counts all the entities of the specified type matching the specified values.
+		/// </summary>
+		/// <param name="type">The type of entity to retrieve.</param>
+		/// <param name="parameters">The parameters to query with.</param>
+		/// <returns></returns>
+		int CountEntities<T>(Dictionary<string, object> parameters)
+			where T : IEntity;
+		
+		/// <summary>
+		/// Counts all the entities of the specified type from the data store.
+		/// </summary>
+		/// <param name="group">The group of filters to apply to the query.</param>
+		/// <returns>The total number of entities found.</returns>
+		int CountEntities(IDataFilterGroup group);
+		
+		int CountEntitiesWithReference<T>(string propertyName, Type referencedEntityType, Guid referencedEntityID)
+			where T : IEntity;
+		
+		/// <summary>
+		/// Counts all the entities of the specified type with a reference to any of the provided entities.
+		/// </summary>
+		/// <param name="propertyName">The name of the property containing the reference.</param>
+		/// <param name="referencedEntities">An array of entities to check the reference to.</param>
+		/// <returns>The total number of entities found.</returns>
+		int CountEntitiesWithReference<T>(string propertyName, IEntity[] referencedEntities)
+			where T : IEntity;
+		
+		int CountEntities(Type type, string propertyName, object propertyValue);
+		
+		int CountEntities(Type type, IDataFilterGroup filterGroup);
+		
+		int CountEntities<T>(IDataFilterGroup filterGroup)
+			where T : IEntity;
+		
+		/// <summary>
+		/// Counts all the entities of the specified type from the data store.
+		/// </summary>
+		/// <param name="filter">The filter to apply to the query.</param>
+		/// <returns>The total number of entities found.</returns>
+		int CountEntities(IDataFilter filter);
+	}
+}
