0df2d3da71a334cd86382bc7cbc7298fbbdd1de4
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-11-02 02:42:16 +0000

    Added CreateAuthoredReactionTests

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/AssemblyScanInfo.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/AssemblyScanInfo.cs
@@ -1,4 +1,6 @@
 ﻿using SoftwareMonkeys.SiteStarter.Business;
+using SoftwareMonkeys.SiteStarter.Entities;
 
+[assembly: AssemblyContainsEntities(true)]
 [assembly: AssemblyContainsStrategies(true)]
 [assembly: AssemblyContainsReactions(true)]
