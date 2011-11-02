349dcdd3bfe4adfbf5e983fe523ed88d384c7bbb
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-11-02 02:41:09 +0000

    Added reaction to create strategy Added IAuthored interface - has Author property Added CreateAuthoredReaction - sets the entity.Author property to the current user (of any entity which implements IAuthored created via CreateStrategy)

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business/CreateAuthoredReaction.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using SoftwareMonkeys.SiteStarter.Business.Security;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Reacts to the creation of any authored entity (ie. an entity with an author).
+	/// </summary>
+	[Reaction("Create", "IAuthored")]
+	public class CreateAuthoredReaction : BaseCreateReaction
+	{
+		public CreateAuthoredReaction()
+		{
+		}
+		
+		public override void React(SoftwareMonkeys.SiteStarter.Entities.IEntity entity)
+		{		
+			IAuthored authoredEntity = (IAuthored)entity;
+			
+			// Set the author of the entity
+			authoredEntity.Author = AuthenticationState.User;
+			
+			base.React(entity);
+		}
+	}
+}
---
+++ Src/App/SoftwareMonkeys.SiteStarter.Contracts/Entities/IAuthored.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareMonkeys.SiteStarter.Entities
+{
+	/// <summary>
+	/// Defines the interface for entities that have an author. The author generally has ownership and the rights associated with it.
+	/// </summary>
+	public interface IAuthored : IEntity
+	{
+		IUser Author { get;set; }
+	}
+}
