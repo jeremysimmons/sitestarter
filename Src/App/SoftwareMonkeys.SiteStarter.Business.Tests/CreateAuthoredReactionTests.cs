33706df2b9d669c352e00da27d09804cbf9f993e
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-11-02 02:42:16 +0000

    Added CreateAuthoredReactionTests

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/CreateAuthoredReactionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using SoftwareMonkeys.SiteStarter.Business.Security;
+using SoftwareMonkeys.SiteStarter.Entities;
+using SoftwareMonkeys.SiteStarter.Entities.Tests.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class CreateAuthoredReactionTests : BaseBusinessTestFixture
+	{
+		public CreateAuthoredReactionTests()
+		{
+		}
+		
+		[Test]
+		public void Test_React()
+		{
+			IUser author = new User();
+			author.ID = Guid.NewGuid();
+			
+			AuthenticationState.User = (User)author;
+			
+			MockAuthoredEntity entity = CreateStrategy.New<MockAuthoredEntity>(false).Create<MockAuthoredEntity>();
+			
+			IUser foundAuthor = entity.Author;
+			
+			Assert.IsNotNull(foundAuthor, "No author assigned.");
+			
+			Assert.AreEqual(author.ID.ToString(), foundAuthor.ID.ToString(), "IDs didn't match.");
+		}
+	}
+}
