9383b5d7fcce8ba6fbe0c6607ab446985d693b33
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-11-12 04:08:41 +0000

    Renamed IUniqueValidateStrategy to IValidateUniqueStrategy

---
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Contracts/Business/IValidateUniqueStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Business
+{
+	/// <summary>
+	/// Defines the interface of all "unique validation" (ie. ensuring a specific property of an entity is unique) strategies.
+	/// </summary>
+	public interface IValidateUniqueStrategy : IValidatePropertyStrategy
+	{
+		
+	}
+}
