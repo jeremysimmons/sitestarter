1cbecd33d65bd4fdc2a593c574ffde3031a26234
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-11-13 08:27:52 +0000

    Tweaked save and update strategy tests

--- trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/SaveStrategyTests.cs
+++ trunk/Src/App/SoftwareMonkeys.SiteStarter.Business.Tests/SaveStrategyTests.cs
@@ -32,32 +32,27 @@ namespace SoftwareMonkeys.SiteStarter.Business.Tests
 			Assert.IsNotNull(foundArticle);
 		}
 		
-		
 		[Test]
 		public void Test_Save_InvalidEntityMustNotSave()
 		{
 			// Create the mock entity
-			TestUser user = CreateStrategy.New<TestUser>(false).Create<TestUser>();
-			user.ID = Guid.NewGuid();
-			user.FirstName = "Test";
-			user.LastName = "User";
+			MockInvalidEntity entity = new MockInvalidEntity();
+						
+			entity.Validator = new ValidateStrategy();
 			
-			// Set a mock validator that will always fail
-			user.Validator = new MockInvalidValidateEntityStrategy();
-			
-			Assert.IsFalse(user.IsValid, "The validator returned true when it should return false.");
+			Assert.IsFalse(entity.IsValid, "The validator returned true when it should return false.");
 			
 			// Try to save the entity
-			bool isValid = SaveStrategy.New(user, false).Save(user);
+			bool isValid = SaveStrategy.New(entity, false).Save(entity);
 			
 			// Ensure that the save was rejected
 			Assert.IsFalse(isValid, "The save strategy didn't recognise the entity as invalid.");
 			
-			// Try loading the user from the data store to see if it's found
-			TestUser foundUser = RetrieveStrategy.New<TestUser>(false).Retrieve<TestUser>("ID", user.ID);
+			// Try loading the entity from the data store to see if it's found
+			MockInvalidEntity foundEntity = RetrieveStrategy.New<MockInvalidEntity>(false).Retrieve<MockInvalidEntity>("ID", entity.ID);
 			
-			// Ensure the user wasn't found and therefore wasn't saved
-			Assert.IsNull(foundUser, "The entity was found in the store even though it shouldn't have saved.");
+			// Ensure the entity wasn't found and therefore wasn't saved
+			Assert.IsNull(foundEntity, "The entity was found in the store even though it shouldn't have saved.");
 		}
 		
 		[Test]
