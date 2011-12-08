8c8ca7c0304b2db115815611a03736c0fe66fbd0
johnrebbeck <johnrebbeck@78604c24-c2bf-11dd-8bad-cde060d8fd3c>
2011-12-08 06:57:32 +0000

    Added ViewSimpleEntityElement

---
+++ Src/App/SoftwareMonkeys.SiteStarter.Web/Elements/ViewSimpleEntityElement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SoftwareMonkeys.SiteStarter.Entities;
+
+namespace SoftwareMonkeys.SiteStarter.Web.Elements
+{
+	/// <summary>
+	///
+	/// </summary>
+	[Element("View", "ISimple")]
+	public class ViewSimpleEntityElement : BaseElement
+	{
+		private ISimple dataSource;
+		public ISimple DataSource
+		{
+			get { return dataSource; }
+			set { dataSource = value; }
+		}
+		
+		private string headingText = String.Empty;
+		public string HeadingText
+		{
+			get {
+				if (headingText == String.Empty && DataSource != null)
+					headingText = DataSource.ShortTypeName;
+				return headingText; }
+			set { headingText = value; }
+		}
+		
+		public ViewSimpleEntityElement()
+		{
+		}
+		
+		protected override void CreateChildControls()
+		{
+			Table table = new Table();
+			table.Width = Width;
+			table.Rows.Add(new TableRow());
+			table.Rows[0].Cells.Add(new TableCell());
+			table.Rows[0].Cells[0].CssClass = "Heading2";
+			table.Rows[0].Cells[0].Width = Unit.Percentage(100);
+			table.Rows[0].Cells[0].ColumnSpan = 2;
+			table.Rows[0].Cells[0].Controls.Add(new LiteralControl(HeadingText));
+			
+			if (DataSource != null)
+			{
+				table.Rows.Add(new TableRow());
+				table.Rows[1].Cells.Add(new TableCell());
+				table.Rows[1].Cells[0].Controls.Add(new LiteralControl(DataSource.Title));
+				
+				table.Rows.Add(new TableRow());
+				table.Rows[2].Cells.Add(new TableCell());
+				table.Rows[2].Cells[0].Controls.Add(new LiteralControl(DataSource.Description));
+			}
+			
+			Controls.Add(table);
+			
+			base.CreateChildControls();
+		}
+	}
+}
